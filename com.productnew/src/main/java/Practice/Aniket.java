67cc664fcbc37baee1bbc2140d951673da8f5447
DELL <DELL@DESKTOP-GCFA0QV.mshome.net>
2023-09-19 15:20:54 +0530

    aniket the coder

--- com.productnew/src/main/java/Practice/Aniket.java
+++ com.productnew/src/main/java/Practice/Aniket.java
@@ -4,6 +4,7 @@ public class Aniket {
 	public static void main(String[] args) {
 		System.out.println("Aniket will code in this class");
 		System.out.println("Senior i am coding here from today");
+		System.out.println("I am aniket");
 	}
 
 }
