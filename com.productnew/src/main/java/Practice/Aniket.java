c42f5c687af9f3c5f4595f8c1d5496bad5c12412
sagar nikam <sagar.n@hashtechy.com>
2023-09-20 09:50:38 +0530

    latest code

--- com.productnew/src/main/java/Practice/Aniket.java
+++ com.productnew/src/main/java/Practice/Aniket.java
@@ -3,10 +3,8 @@ package Practice;
 public class Aniket {
 	public static void main(String[] args) {
 		System.out.println("Aniket will code in this class");
-		System.out.println("Senior i am coding here from today");
-		System.out.println("I am aniket");
-		System.out.println("Papa kehte hai bada naam karega");
-		inhuhnuij9o
+		
+		
 	}
 
 }
