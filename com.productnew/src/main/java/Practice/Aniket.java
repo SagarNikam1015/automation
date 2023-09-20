1cb593a61d56b0f1599954b86ea371207b7fcccf
sagar nikam <sagar.n@hashtechy.com>
2023-09-20 15:06:29 +0530

    new test method implement

--- com.productnew/src/main/java/Practice/Aniket.java
+++ com.productnew/src/main/java/Practice/Aniket.java
@@ -5,6 +5,8 @@ import org.bouncycastle.util.test.Test;
 public class Aniket {
 	public static void main(String[] args) {
 		
+		Aniket a=new Aniket();
+		a.test();
 		System.out.println("Senior i am coding here from today");
 		System.out.println("I am aniket");
 		System.out.println("Papa kehte hai bada naam karega");
@@ -15,7 +17,10 @@ public class Aniket {
 	}
 	
 	
-	
+	public void test() {
+		System.out.println("New Test Method is Implemented");
+		System.out.println("Ok");
+	}
 	
 	
 	
